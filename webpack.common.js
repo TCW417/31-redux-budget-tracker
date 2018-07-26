cb11a78539823edc14d9f55d383d1a8285fc34b2
Tracy Williams <selpilot@gmail.com>
2018-07-26 16:16:28 -0700

    final tweaks to webpack.common and _base.scss

--- webpack.common.js
+++ webpack.common.js
@@ -17,7 +17,7 @@ webpackConfig.output = {
 
 webpackConfig.plugins = [
   new HtmlWebpackPlugin({
-    title: '401d25 Redux',
+    title: 'Budget Tracker',
   }),
   new DefinePlugin({
     API_URL: JSON.stringify(process.env.API_URL),
